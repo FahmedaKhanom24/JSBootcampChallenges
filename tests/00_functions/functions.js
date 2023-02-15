01062eab54525b7489a6c3c5e0e60bef9975bbee
Touheda Khanom <FahmedaKhanom15@gmail.com>
2023-02-14 21:16:58 -0500

    Some new changes in fuctions exercises

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -1,11 +1,25 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable consistent-return */
-function max() {
+function max(num1,num2,num3) {
+  if (num1>num2 && num1>num3){
+    return num1
+  }
+  else if (num2>num1 && num2>num3){
+    return num2
+  }
+ 
+  else{
+    return num3
+  }
 
 }
 
-function addUnknown() {
-
+function addUnknown(num1,num2,num3=0,num4=0,num5=0) {
+  if (num3===undefined){
+    num3=0
+  }
+  
+  return num1+num2+num3+num4+num5
 }
 
 function evenOrOdd() {
